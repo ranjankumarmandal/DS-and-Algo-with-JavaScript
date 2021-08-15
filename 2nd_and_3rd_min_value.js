ea689c5deadf67b4e687e9e4dbed091bf048b1ee
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-08-15 17:01:01 +0530

    matrix practice

---
+++ 2nd_and_3rd_min_value.js
@@ -0,0 +1,33 @@
+
+function solution(array) {
+    let result = [];
+    var MAX = 10000;
+    let n = array.length;
+    let firstmin = MAX, secmin = MAX, thirdmin = MAX;
+    for (let i = 0; i < n; i++)
+    {
+        if (array[i] < firstmin)
+        {
+            thirdmin = secmin;
+            secmin = firstmin;
+            firstmin = array[i];
+        }
+ 
+        else if (array[i] < secmin)
+        {
+            thirdmin = secmin;
+            secmin = array[i];
+        }
+ 
+        else if (array[i] < thirdmin)
+            thirdmin = array[i];
+    }
+
+    result.push(secmin);
+    result.push(thirdmin);
+    return result;
+
+}
+
+console.log(solution([11, 3, 12, 544, 35, 30, 34]));
+
