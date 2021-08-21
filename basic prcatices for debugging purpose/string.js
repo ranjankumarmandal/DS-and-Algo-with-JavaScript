5bdfb0304c252eddcfef22f5877cade23ac6f39b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-08-21 07:36:20 +0530

    string

---
+++ basic prcatices for debugging purpose/string.js
@@ -0,0 +1,18 @@
+//concepts
+let str = 'hello, "hi"';
+console.log(str);
+
+//Strings are immutable
+let mystr = "bob";
+mystr[0] = "j";
+console.log(mystr[0]);
+  
+//-------------
+
+// String Methods
+str = "Crio.do";
+
+console.log(str.charAt(0));
+console.log(str.charCodeAt(0));
+console.log(str.substr(1, 3));
+console.log(str.substring(1, 3));
