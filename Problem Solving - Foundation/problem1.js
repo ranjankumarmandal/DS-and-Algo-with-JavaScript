608f02728187902e6967738ead2a01f0d2b88e1f
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-08-26 12:51:52 +0530

    problem solving 3 v1

--- Problem Solving - Foundation/problem1.js
+++ Problem Solving - Foundation/problem1.js
@@ -55,7 +55,7 @@ function charCount(str) {
     for(let char of str) {
         char = char.toLowerCase();
         if(/[a-z0-9]/.test(char)) {
-            result[char] = ++result[char] || 1;  // OR operator in JS finds the first truthy value
+            result[char] = ++result[char] || 1;  // OR operator in JS finds the first truthy value and And operator finds first falsy value
         }
     }
 
