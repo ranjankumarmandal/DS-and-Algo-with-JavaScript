93dae42d53c3c4d76706b5b51aaade3ed52b7fba
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-08-28 12:46:58 +0530

    frequency counter v3

--- Problem Solving Pattern/Frequence Counter/anagram.js
+++ Problem Solving Pattern/Frequence Counter/anagram.js
@@ -26,5 +26,23 @@ function anagram(str1, str2) {
         // check if indexTemp has -1 value then return false
         // remove str1[i] at str2
     // return true
+
+    if(str1.length != str2.length) {
+        return false;
+    }
+
+    str1 = str1.split("");
+    str2 = str2.split("");
+
+    for(let i = 0; i < str1.length; i++) {
+        let indexTemp = str2.indexOf(str1[i]);
+        if(indexTemp === -1) {
+            return false;
+        }
+        str2.splice(indexTemp, 1);
+    }
+
+    return true;
 }
 
+anagram("", "")
