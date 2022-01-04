f691ed854a76da89d57c9101947ef79c2c6c7d5b
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-04 10:14:19 +0530

    problem solving approach steps

--- Frontend Interview Questions - Problem Solving/amazon_interview.js
+++ Frontend Interview Questions - Problem Solving/amazon_interview.js
@@ -1,3 +1,8 @@
 // find solution for -    sum(1)(2)(3)(4)();
 
-// Problem solving approach
+// Problem Solving approach
+// 1. Understand the problem
+// 2. Explore the concrete examples
+// 3. Break it down
+// 4. Solve / simplify
+// 5. Look back & refactor
