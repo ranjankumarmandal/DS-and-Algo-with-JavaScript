692df2a655de985d20b21b990a2a82b005d617e5
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-04 10:27:45 +0530

    solution 1

--- Frontend Interview Questions - Problem Solving/amazon_interview.js
+++ Frontend Interview Questions - Problem Solving/amazon_interview.js
@@ -14,3 +14,18 @@ sum(1)(2)(3)(6)();  // output: 12
 */
 
 // We can achieve this problem by usig 'function currying' concept in JavaScript.
+
+// solution 1
+let sum = function (a) {
+  return function (b) {
+    return function (c) {
+      return function (d) {
+        return function () {
+          return a + b + c + d;
+        };
+      };
+    };
+  };
+};
+
+sum(1)(2)(3)(4)();
