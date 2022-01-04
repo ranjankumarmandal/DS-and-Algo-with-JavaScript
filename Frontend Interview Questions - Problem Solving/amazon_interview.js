4326ee7e0d8eedc67be06ee0036ad71f0a1c9bda
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-04 10:17:39 +0530

    understand the problem and explore the concrete examples

--- Frontend Interview Questions - Problem Solving/amazon_interview.js
+++ Frontend Interview Questions - Problem Solving/amazon_interview.js
@@ -6,3 +6,9 @@
 // 3. Break it down
 // 4. Solve / simplify
 // 5. Look back & refactor
+
+/*
+sum(1)(2)(3)(4)();  // output: 10
+sum(1)(2)(3)(5)();  // output: 11
+sum(1)(2)(3)(6)();  // output: 12
+*/
