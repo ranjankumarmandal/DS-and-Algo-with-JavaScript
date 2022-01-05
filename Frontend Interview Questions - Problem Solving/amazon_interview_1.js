7b3528b81f99f3dc8d44d82b6caa2e8fdcbb5936
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-05 10:05:18 +0530

    solution 1

--- Frontend Interview Questions - Problem Solving/amazon_interview_1.js
+++ Frontend Interview Questions - Problem Solving/amazon_interview_1.js
@@ -15,8 +15,15 @@ rev('hello there hope all is well') // well is all hope there hello
 /*
 function rev(str) {
   // convert string to array
-  // replace the first place with last place and so on 
-  // convert the arr to result string
+  // replace the first place with last place and so on in a result string
   // return the result string
 }
 */
+
+// solution 1
+function rev(str) {
+  let arr = str.split(' ');
+  let result = '';
+  for (let i = arr.length; i >= 0; i--) result += arr[i] + ' ';
+  return result;
+}
