46ddddc5b7d40810566db0a2a4084dfaeed32b8d
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-01-05 09:34:06 +0530

    problem solving approach steps

--- Frontend Interview Questions - Problem Solving/amazon_interview_1.js
+++ Frontend Interview Questions - Problem Solving/amazon_interview_1.js
@@ -1,3 +1,8 @@
 // Problem - reverse words in a given string
 
 // Problem Solving approach -
+// 1. Understand the problem
+// 2. Explore the concrete examples
+// 3. Break it down
+// 4. Solve / simplify
+// 5. Look back & refactor
